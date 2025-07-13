1a9679b71ccf25040be47f21bf62dbb850695734
Harshitha-Nithyanandhan <harshitha9000k@gmail.com>
2025-07-13 07:05:54 +0700

    Edited ft_exit.c to match bash output

--- src/built-ins/ft_exit.c
+++ src/built-ins/ft_exit.c
@@ -2,9 +2,10 @@
 
 static int	is_numeric(const char *str)
 {
-	int	i = 0;
+	int	i;
 
-	if (!str)
+	i = 0;
+	if (!str || str[0] == '\0')
 		return (0);
 	if (str[i] == '+' || str[i] == '-')
 		i++;
@@ -17,31 +18,36 @@ static int	is_numeric(const char *str)
 	return (1);
 }
 
-static void	cleanup_and_exit(t_program *minishell, int exit_code)
+static void	cleanup_and_exit(t_program *minishell, int code)
 {
 	free_shell(minishell);
-	exit(exit_code);
+	exit(code);
 }
 
 int	ft_exit(t_command *cmd, t_program *minishell)
 {
-	int	exit_code;
+	int	code;
 
-	ft_putendl_fd("exit", STDERR_FILENO);
 	if (!cmd->args[1])
+	{
+		ft_putendl_fd("exit", STDERR_FILENO);
 		cleanup_and_exit(minishell, minishell->error_code);
+	}
 	if (!is_numeric(cmd->args[1]))
 	{
-		print_error_message(ERR_SYNTAX_TOKEN, cmd->args[1], minishell);
+		ft_putstr_fd("exit\nminishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(cmd->args[1], STDERR_FILENO);
+		ft_putendl_fd(": numeric argument required", STDERR_FILENO);
 		cleanup_and_exit(minishell, 255);
 	}
 	if (cmd->args[2])
 	{
-		print_error_message(ERR_SYNTAX_TOKEN, "too many arguments", minishell);
+		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
 		minishell->error_code = 1;
 		return (1);
 	}
-	exit_code = ft_atoi(cmd->args[1]);
-	cleanup_and_exit(minishell, (unsigned char)exit_code);
-	return (0); // unreachable, but makes compiler happy xD
+	code = ft_atoi(cmd->args[1]);
+	ft_putendl_fd("exit", STDERR_FILENO);
+	cleanup_and_exit(minishell, (unsigned char)code);
+	return (0);
 }
