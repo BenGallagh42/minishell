d4b6754bb3b60b637f3207459a87c7f19b8d5514
harshitha-nithyanandhan <harshitha9000k@gmail.com>
2025-07-14 21:35:30 +0000

    Fixed redirection errors exec_main.c & exec_redirection.c

--- obj/exec_redirection.d
+++
@@ -1,6 +0,0 @@
-obj/exec_redirection.o: src/exec/exec_redirection.c inc/minishell.h \
-  inc/../libft/inc/libft.h
-
-inc/minishell.h:
-
-inc/../libft/inc/libft.h:
--- obj/ft_export2.d
+++
@@ -1,6 +0,0 @@
-obj/ft_export2.o: src/built-ins/ft_export2.c inc/minishell.h \
-  inc/../libft/inc/libft.h
-
-inc/minishell.h:
-
-inc/../libft/inc/libft.h:
